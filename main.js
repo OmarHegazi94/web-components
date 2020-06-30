cd56b7f94f6fea0c8b430fdc9f98239f72b9174b
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-30 13:09:49 +0200

    added alert

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
 const totalExpensesOutput = document.querySelector("#total-expenses");
+// const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -11,6 +12,17 @@ const clear = () => {
     reasonInput.value = '';
     amountInput.value = '';
 };
+
+function presentAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'my-custom-class';
+    alert.header = 'Invalid Inputs';
+    alert.message = 'Please enteer valid reason and amount';
+    alert.buttons = ['OK'];
+  
+    document.body.appendChild(alert);
+    return alert.present();
+}
 
 confirmBtn.addEventListener("click", function() {
     const enteredReason = reasonInput.value;
@@ -20,6 +32,14 @@ confirmBtn.addEventListener("click", function() {
         enteredReason.trim().length <= 0 || 
         enteredAmount <= 0 || enteredAmount.trim().length <= 0
         ) {
+            presentAlert();
+            // alertCtrl.message = 'Please enteer valid reason and amount';
+            // alertCtrl.header = 'Invalid Inputs';
+            // alertCtrl.buttons = ['Okay'];
+            // return alertCtrl.present();
+            // .then(alertElement => {
+            //     alertElement.present();
+            // });
         return;
     }
 
